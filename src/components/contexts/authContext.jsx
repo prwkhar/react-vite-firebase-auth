74a03b715bc0cdecc1e244a7e3aff5cfe48f1191
Prakhar Agarwal <prakharagarwal1434@gmail.com>
2025-02-05 17:59:24 +0530

    first

--- src/components/contexts/authContext.jsx
+++ src/components/contexts/authContext.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { auth } from "../firebase";
+import { auth } from "../Firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, sendPasswordResetEmail, EmailAuthProvider, reauthenticateWithCredential, updateEmail, updatePassword } from "firebase/auth";
 
 const AuthContext = React.createContext();
